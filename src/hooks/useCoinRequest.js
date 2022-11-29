d77a1c0c5a79042e8416f803f2f6575f38600d7b
Les Ansley <lesansley@protonmail.ch>
2022-11-29 17:17:17 +0200

    Fetch refresh (only for 1st 10 items)

--- src/hooks/useCoinRequest.js
+++ src/hooks/useCoinRequest.js
@@ -2,16 +2,19 @@ import useSWRInfinite from "swr/infinite";
 
 //TODO: Put into a config file
 const baseUrl = "https://api.coingecko.com/api/v3";
+const refreshInterval = 15000;
+const perPage = 12;
 
 export const useCoinRequest = (path) => {
   if (!path) {
     throw new Error("Path is required");
   }
-
   const url = baseUrl + path;
   const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
-    (index) => `${url}&per_page=12&page=${index + 1}`
+    (index) => `${url}&per_page=${perPage}&page=${index + 1}`,
+    { refreshInterval: refreshInterval }
   );
+  console.log(data);
   return { data, error, mutate, size, setSize, isValidating };
 };
 
