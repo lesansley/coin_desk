698ad2309576a17e3b7ec32df18a6125ae9e935b
Les Ansley <lesansley@protonmail.ch>
2022-12-01 19:13:43 +0200

    Add get currency api call

---
+++ src/hooks/useAllCurrencyRequest.js
@@ -0,0 +1,11 @@
+import useSWR from "swr";
+
+import { BASE_URL } from "../config";
+
+export const useAllCurrencyRequest = () => {
+  const url = `${BASE_URL}/simple/supported_vs_currencies`;
+  const { data, error, isValidating } = useSWR(() => url);
+  return { data, error, isValidating };
+};
+
+export default useAllCurrencyRequest;
