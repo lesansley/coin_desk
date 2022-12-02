1b08794f2e771f4e7eef1c93aa883ef4718e1ac7
Les Ansley <lesansley@protonmail.ch>
2022-12-02 13:18:43 +0200

    Add information panel

--- src/components/coin-info.js
+++ src/components/coin-info.js
@@ -1,16 +1,16 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { sanitize } from "dompurify";
+import { Spinner } from "reactstrap";
 import useCoinRequest from "../hooks/useCoinRequest";
-import CoinSummary from "./coin-summary";
 
-function CoinInfo({ coin }) {
-  const result = useCoinRequest(coin);
-
-  return (
-    <>
-      <div>Coin Info Component for {coin}</div>
-      <CoinSummary />
-    </>
-  );
+function CoinInfo() {
+  const coin = useParams();
+  const { data, error } = useCoinRequest(coin.id);
+  if (error) return <h3>Oops. Something went wrong!</h3>;
+  if (!data) return <Spinner>Loading...</Spinner>;
+  const description = data.description.en;
+  return <div dangerouslySetInnerHTML={{ __html: sanitize(description) }} />;
 }
 
 export default CoinInfo;
